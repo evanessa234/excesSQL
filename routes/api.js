8c663e936171604ee6f2b9c55b10ab36f1418751
aayush <aayushdongre4@gmail.com>
2019-12-26 08:15:19 +0530

    Fetch all messages for a certain id

--- routes/api.js
+++ routes/api.js
@@ -79,26 +79,59 @@ router.get('/message/history/:sdrn', async (req, res) => {
 });
 
 /**
- * Fetch messages for respective `identification` and `roll number` || `sdrn`
+ * Fetch all messages for respective `identification`
  * type: GET
  * query params = []
  * URL: /message/fetch/:identification/:(roll or sdrn)
  */
-router.get('/message/fetch/:identification/:roll', async (req, res) => {
+router.get('/message/fetch/:identification/all', async (req, res) => {
   const conn = await db();
   try {
     await conn.query('START TRANSACTION');
-    let result;
-    if (req.params.roll === 'all') {
-      result = await conn.query(
-        'select * from `recieved` where  `identification`=?', [req.param.identification],
-      );
-    } else {
-      result = await conn.query(
-        `select * from \`recieved\` where \`to\` = '${req.params.roll}'`,
-      );
-    }
+    const result = await conn.query(
+      'select * from `recieved` where  `identification`= ?', [req.params.identification],
+    );
 
+    res.status(200).json({
+      result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  } finally {
+    await conn.release();
+    await conn.destroy();
+  }
+});
+
+
+/**
+ * Fetch all messages for respective `id (roll or sdrn)`
+ * type: GET
+ * query params = []
+ * URL: /message/fetch/:(roll or sdrn)/all
+ */
+router.get('/message/fetch/single/:id/all', async (req, res) => {
+  const conn = await db();
+  try {
+    await conn.query('START TRANSACTION');
+    const resp = await conn.query(
+      'select `topics` from `users` where  `id` =  ? ', [req.params.id],
+    );
+    const topics = resp[0].topics.split('_');
+    let val = '';
+    let query = 'select * from `recieved` where identification in (';
+
+    for (let i = 0; i < topics.length; i += 1) {
+      if (i === 0) val += `${topics[i]}`;
+      else val += `_${topics[i]}`;
+
+      if (i === topics.length - 1) query += `'${val}')`;
+      else query += `'${val}',`;
+    }
+    query += ' ORDER by timestamp DESC';
+    const result = await conn.query(query);
     res.status(200).json({
       result,
     });
